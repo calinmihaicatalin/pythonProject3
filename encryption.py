a97e9e91e7848b9660ea6524efddf2cb59b31cab
Mihai <jaggermihai@gmail.com>
2024-10-16 19:11:44 +0300

    -

--- encryption.py
+++ encryption.py
@@ -7,8 +7,8 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key  : {key}")
+#print(f"chars: {chars}")
+#print(f"key  : {key}")
 
 #ENCRYPT
 
@@ -21,3 +21,15 @@ for letters in plain_text:
 
 print(f"original message : {plain_text}")
 print(f"encrypted message: {cipher_text}")
+
+#DECRYPT
+
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
+
+for letters in cipher_text:
+    index = key.index(letters)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message : {plain_text}")
